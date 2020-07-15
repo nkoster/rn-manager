3632260506688d52713a1ba47682f62eb44d26a7
Niels Koster <n.koster@portavita.eu>
2020-07-15 22:35:17 +0200

    Add passwordChanged stuff

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,9 +1,14 @@
 import { _emailChanged } from '../actions/types'
+import { _passwordChanged } from '../actions/types'
 
-export default (state = { email: '' }, action) => {
+const initialState = { email: '', password: '' }
+
+export default (state = initialState, action) => {
     switch(action.type) {
         case _emailChanged:
             return { ...state, email: action.payload }
+        case _passwordChanged:
+            return { ...state, password: action.payload }
         default:
             return state
     }
