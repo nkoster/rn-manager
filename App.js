3deac48690598ad8fb09cb9c8e28798a55c06e41
Niels Koster <n.koster@portavita.eu>
2020-07-16 00:05:01 +0200

    applyMiddleware(ReduxThunk)

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import ReduxThunk from 'redux-thunk'
 import reducers from './src/reducers'
 import firebase from 'firebase'
 import firebaseConfig from './firebase'
@@ -12,8 +12,9 @@ class App extends Component {
       firebase.initializeApp(firebaseConfig)
   }
   render() {
+    const store = createStore(reducers, applyMiddleware(ReduxThunk))
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     )
