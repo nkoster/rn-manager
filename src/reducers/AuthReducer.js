c7cbaa079cb4dd0c09e8a458671c328ee7cd599b
Niels Koster <n.koster@portavita.eu>
2020-07-17 23:18:33 +0200

    Add loginFailed

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,8 +1,8 @@
 import {
-    _emailChanged, _passwordChanged, _loginSuccess
+    _emailChanged, _passwordChanged, _loginSuccess, _loginFailed
 } from '../actions/types'
 
-const initialState = { email: '', password: '', user: null }
+const initialState = { email: '', password: '', user: null, error: '' }
 
 export default (state = initialState, action) => {
     console.log(action)
@@ -12,7 +12,9 @@ export default (state = initialState, action) => {
         case _passwordChanged:
             return { ...state, password: action.payload }
         case _loginSuccess:
-            return { ...state, user: action.payload }
+            return { ...state, user: action.payload, error: '' }
+        case _loginFailed:
+            return { ...state, error: action.payload, password: '' }
         default:
             return state
     }
