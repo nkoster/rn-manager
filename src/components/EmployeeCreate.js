6da731ae4376f4a92e80b4e616e18867d2d730d5
Niels Koster <n.koster@portavita.eu>
2020-07-19 22:56:14 +0200

    Throw in some input stuff

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
+import { Card, CardSection, Input, Button } from './common'
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <div>App</div>
+            <Card>
+                <CardSection>
+                    <Input
+                        label='Name'
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        label='Phone'
+                    />
+                </CardSection>
+                <CardSection>
+
+                </CardSection>
+                <CardSection>
+                    <Button>Save</Button>
+                </CardSection>
+            </Card>
         )
     }
 }
