b5323092c7cc93ecf2987b75a7693e42f1f54531
Niels Koster <n.koster@portavita.eu>
2020-07-19 22:32:23 +0200

    Initial

---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class EmployeeCreate extends Component {
+    render() {
+        return (
+            <div>App</div>
+        )
+    }
+}
+
+export default EmployeeCreate
