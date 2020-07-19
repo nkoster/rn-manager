489d2565b62cf82c04bd45aee745bfaff11bbff7
Niels Koster <n.koster@portavita.eu>
2020-07-19 22:56:50 +0200

    Initial

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,12 @@
+import { 
+    _employeeUpdate
+} from '../actions/types'
+
+const initialState = {}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
