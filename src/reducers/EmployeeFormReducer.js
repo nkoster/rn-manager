bb70d7b495aa8c080c6bbd2d7a7895c46fd2af00
Niels Koster <n.koster@portavita.eu>
2020-07-19 23:45:58 +0200

    _employeeUpdate and initialState

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,11 +1,17 @@
 import { 
-    _employeeUpdate
+    _employeeUpdate, _emailChanged
 } from '../actions/types'
 
-const initialState = {}
+const initialState = {
+    name: '',
+    phone: '',
+    shift: ''
+}
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case _employeeUpdate:
+            return { ...state, [action.payload.prop]: action.payload.value }
         default:
             return state
     }
