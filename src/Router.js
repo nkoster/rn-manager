bdd9e13f4f2c9ab17f2522dbfaa86362aa5b559d
Niels Koster <n.koster@portavita.eu>
2020-07-19 16:27:02 +0200

    Initial, Router thingy

---
+++ src/Router.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Scene, Router } from 'react-native-router-flux'
+import LoginForm from './components/LoginForm'
+
+const RouterComponent = _ => {
+    return (
+        <Router>
+            <Scene key='root'>
+                <Scene
+                    key='LoginForm'
+                    component={LoginForm}
+                    title='Just do it'
+                />
+            </Scene>
+        </Router>
+    )
+}
+
+export default RouterComponent
