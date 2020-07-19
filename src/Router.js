d88f81d98a99412eb7845b59a4c1fdea64fe6fd2
Niels Koster <n.koster@portavita.eu>
2020-07-19 21:53:55 +0200

    Split Scenes into login and main

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
+import EmployeeList from './components/EmployeeList'
 
 const RouterComponent = _ => {
     return (
-        <Router sceneStyle={{ paddingTop: 65 }}>
-            <Scene key='root'>
-                <Scene
-                    key='LoginForm'
-                    component={LoginForm}
-                    title='Just do it'
-                />
+        <Router sceneStyle={{ paddingTop: 60 }}>
+            <Scene key='root' hideNavBar>
+                <Scene key='auth'>
+                    <Scene
+                        key='login'
+                        component={LoginForm}
+                        initial
+                        title='Please log in'
+                    />
+                </Scene>
+                <Scene key='main'>
+                    <Scene
+                        key='employees'
+                        component={EmployeeList}
+                        title='Employees'
+                    />
+                </Scene>
             </Scene>
         </Router>
     )
