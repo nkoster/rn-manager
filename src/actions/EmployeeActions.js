dd0d4d4a09e4ada3b328ab17677a179d240a251a
Niels Koster <n.koster@portavita.eu>
2020-07-21 21:10:06 +0200

    push data to firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,10 +1,18 @@
 import { 
     _employeeUpdate
 } from './types'
+import firebase from 'firebase'
 
 export const employeeUpdate = ({ prop, value }) => {
     return {
         type: _employeeUpdate,
         payload: { prop, value} 
     }
+}
+
+export const employeeCreate = ({ name, phone, shift }) => {
+    const { currentUser } = firebase.auth()
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+        .push({ name, phone, shift })
+    return { type: '' }
 }
