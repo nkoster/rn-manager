da628234864d862cca197a2c948b2f3a25e03e72
Niels Koster <n.koster@portavita.eu>
2020-07-22 00:41:09 +0200

    render a list

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import _ from 'lodash'
+import { ListView } from 'react-native'
+import { Spinner } from './common'
+import ListItem from './ListItem'
 import { connect } from 'react-redux'
 import { employeesFetch } from '../actions'
 
 class EmployeeList extends Component {
     componentDidMount() {
         this.props.employeesFetch()
+        this.createDataSource(this.props)
+    }
+    componentWillReceiveProps(nextProps) {
+        this.createDataSource(nextProps)
+    }
+    createDataSource({ employees }) {
+        const dataStore = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        })
+        this.dataSource = dataStore.cloneWithRows(employees)
+        console.log('DATASOURCE', this.dataSource)
+    }
+    renderRow(employee) {
+        return (
+            <ListItem employee={employee} />
+        )
     }
     render() {
+        console.log('RENDER', this.props)
+        console.log(this.props.employees)
+        if (!this.props.employees.length)
+            return (
+                <Spinner />
+            )
         return (
-            <View>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-                <Text>EmployeeList</Text>
-            </View>
+            <ListView
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
         )
     }
 }
 
-export default connect(null, {
+const StateProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid }
+    })
+    return { employees }
+}
+
+export default connect(StateProps, {
     employeesFetch
 })(EmployeeList)
