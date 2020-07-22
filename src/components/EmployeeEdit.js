d2003f11beee2979ffdbf0f9e0e59f55b4ceed88
Niels Koster <n.koster@portavita.eu>
2020-07-22 19:58:25 +0200

    Initial

---
+++ src/components/EmployeeEdit.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { employeeUpdate } from '../actions'
+import { Card, CardSection, Button } from './common'
+import EmployeeForm from './EmployeeForm'
+import _ from 'lodash'
+
+class EmployeeEdit extends Component {
+    componentWillMount() {
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({ value, prop })
+        })
+    }
+    onButtonPress() {
+        const { name, phone, shift } = this.props
+        console.log('EDIT', name, phone, shift)
+    }
+    render() {
+        return (
+            <Card>
+                <EmployeeForm />
+                <CardSection>
+                    <Button onPress={this.onButtonPress.bind(this)}>Save</Button>
+                </CardSection>
+            </Card>
+        )
+    }
+}
+
+const stateProps = state => {
+    const { name, phone, shift } = state.employee
+    return {
+        name, phone, shift
+    }
+}
+
+export default connect(stateProps, {
+    employeeUpdate
+})(EmployeeEdit)
