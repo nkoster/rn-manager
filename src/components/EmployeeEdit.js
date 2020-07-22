d382e2807a82ee85e0c8bd00f9cf262e090e8e50
Niels Koster <n.koster@portavita.eu>
2020-07-22 21:34:26 +0200

    add text message thingy - untested!

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { text as textMessage } from 'react-native-communications'
 import { employeeUpdate, employeeSave } from '../actions'
 import { Card, CardSection, Button } from './common'
 import EmployeeForm from './EmployeeForm'
@@ -11,16 +12,23 @@ class EmployeeEdit extends Component {
             this.props.employeeUpdate({ value, prop })
         })
     }
-    onButtonPress() {
+    onSavePress() {
         const { name, phone, shift } = this.props
         this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid })
+    }
+    onTextPress() {
+        const { phone, shift } = this.props
+        textMessage(phone, `Yo you work ${shift}`)
     }
     render() {
         return (
             <Card>
                 <EmployeeForm />
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>Save</Button>
+                    <Button onPress={this.onSavePress.bind(this)}>Save</Button>
+                </CardSection>
+                <CardSection>
+                    <Button onPress={this.onTextPress.bind(this)}>Text Schedule</Button>
                 </CardSection>
             </Card>
         )
